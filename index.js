055c3178e2ebfaf25d0cfc1d9a8d4d124a387b21
maheshverma123 <110168032+maheshverma123@users.noreply.github.com>
2024-12-17 18:41:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 var http = require('http');
+const { exec } = require('child_process');  // To run sonar-scanner
 
+// Create the server
 var server = http.createServer(function(request, response) {
 
+    // Respond to HTTP requests
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!, Welcome To RSA!!");
 
 });
 
+// Start the server
 var port = 80;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+// Run SonarQube analysis after the server is up and running
+exec('sonar-scanner -Dsonar.projectKey=my-project -Dsonar.sources=./ -Dsonar.host.url=$SONARQUBE_HOST', (err, stdout, stderr) => {
+    if (err) {
+        console.error('Error executing sonar-scanner:', err);
+        return;
+    }
+    console.log('SonarQube analysis complete:', stdout);
+    console.error(stderr);
+});
